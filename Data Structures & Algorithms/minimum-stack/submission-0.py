7da571eda1c9f525c8e310e4831157c59bc280b6
Amit Kumar Bind <156566212+Akm1923@users.noreply.github.com>
2026-06-12 10:17:33 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,17 @@
+class MinStack:
+
+    def __init__(self):
+        self.s=[]
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+
+    def pop(self) -> None:
+        return self.s.pop() if len(self.s)!=0 else 0
+
+    def top(self) -> int:
+        return self.s[-1]
+
+    def getMin(self) -> int:
+        return min(self.s)
+        
