b20e4ba11c871861bfbde95110fa844e644a3e44
Amit Kumar Bind <156566212+Akm1923@users.noreply.github.com>
2026-06-12 14:58:38 +0530

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.py
@@ -0,0 +1,15 @@
+from collections import deque
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = deque()
+        res = [0]*len(temperatures)
+
+        for i in range(len(temperatures)-1,-1,-1):
+            while len(stack)!=0 and stack[-1][0]<=temperatures[i]:
+                stack.pop()
+            if len(stack)!=0 and temperatures[i]<=stack[-1][0]:
+                res[i]=stack[-1][1]-i
+            elif len(stack)==0:
+                res[i]=0
+            stack.append((temperatures[i],i))
+        return res        
